4edf1fa77da769d10dafb43fa8b8d71e725d7183
ArTeGaS-2 <probablytorvik@gmail.com>
2025-06-19 20:01:16 +0300

    Генератор сходів

--- Assets/Create_Steps.cs
+++ Assets/Create_Steps.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Create_Steps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject firstStep; // Положення першої сходинки
+    public GameObject stepPrefab; // Шаблон сходинки
+    public int stepNum; // Кількість сходинок яку треба створити
+    public float stepHeight = 0.5f; // Висота сходинки
+    public float stepLenght = 1f; // Довжина сходинки
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < stepNum + 1; i++)
+        {
+            Instantiate(
+                stepPrefab,
+                firstStep.transform.position + new Vector3(
+                    0, i * stepHeight, i * stepLenght),
+                Quaternion.identity);
+        }
     }
 }
