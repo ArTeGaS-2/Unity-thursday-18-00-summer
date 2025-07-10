cc6e2ba6dbeb06ecd8a9ba44f8bec538e0395acc
ArTeGaS-2 <probablytorvik@gmail.com>
2025-07-10 19:59:54 +0300

    Підготовка префабів і базового генератора рівнія

---
+++ Assets/Base_Level_Generator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_Level_Generator : MonoBehaviour
+{
+    [Header("Налаштування рівня")]
+    public GameObject zeroLevel; // Перша платформа - нульовий поверх
+    public GameObject levelPrefab; // Шаблон рівня
+    public int levelNum; // Номер поточного рівня (для нумерації тексту на стіні)
+    public float levelHeightStep = 7f; // Крок між поверхами
+    [Header("Налаштування Стін")]
+    public GameObject zeroLevelWalls; // Перший шар стін
+    public GameObject levelWallsPrefab; // Шаблон стін
+    public float wallsHeightStep = 7f; // Крок між стінами
+    [Header("Інше")]
+    public int levelsNumToGenerate = 10; // Кількість рівнів
+    private void Start()
+    {
+        for (int i = 0; i < levelsNumToGenerate + 1; i++)
+        {
+            Instantiate(levelPrefab,
+                zeroLevel.transform.position + new Vector3(
+                    0, levelNum * levelHeightStep,0),
+                Quaternion.identity);
+        }
+    }
+}
